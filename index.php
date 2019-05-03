976eca5060957e3b10b3c08e1513aae334a95b3d
Cao The Giang <giangctgch17142@fpt.edu.vn>
2019-05-03 14:55:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,19 +4,33 @@
 
 <h1>My first PHP page</h1>
 
-<?php 
+<?php
+ini_set('display_errors', 1);
+echo "Hello abc XYZ!";
+?>
+
+<?php
+$txt1 = "Learn PHP";
+$txt2 = "FPT Greenwich";
+$x = 5;
+$y = 5;
+echo "<h2>" . $txt1 . "</h2>";
+echo "Study PHP at " . $txt2 . "<br>";
+echo $x + $y;
+
+   $db = parse_url(getenv("DATABASE_URL"));
+   $pdo = new PDO("pgsql:" . sprintf(
+        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+        $db["host"],
+        $db["port"],
+        $db["user"],
+        $db["pass"],
+        ltrim($db["path"], "/")
+   ));
+}  
 $sql = "SELECT id, name FROM label";
-$db = parse_url(getenv("DATABASE_URL"));
-$pdo = new PDO("pgsql:" . sprintf(
-    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-    $db["host"],
-    $db["port"],
-    $db["user"],
-    $db["pass"],
-    ltrim($db["path"], "/")
-));
 $stmt = $pdo->prepare($sql);
-//Thi?t l?p ki?u d? li?u tr? v?
+//Thiết lập kiểu dữ liệu trả về
 $stmt->setFetchMode(PDO::FETCH_ASSOC);
 $stmt->execute();
 $resultSet = $stmt->fetchAll();

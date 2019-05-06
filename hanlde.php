cc37cacb573722bf117ee9f1869f34874a2eb6fb
Cao The Giang <giangctgch17142@fpt.edu.vn>
2019-05-06 15:58:26 +0700

    Update hanlde.php

--- hanlde.php
+++ hanlde.php
@@ -2,16 +2,25 @@
    require_once('./dbconnector.php');
    if (isset($_POST['them'])) {
         //them
-    $name=$_POST['Name'];
-    $job=$_POST['Age'];
-    $address=$_POST['Phone'];
-    $salary=$_POST['Email'];
+    $Name=$_POST['Name'];
+    $Age=$_POST['Age'];
+    $Phone=$_POST['Phone'];
+    $Email=$_POST['Email'];
     $sql="Insert Into customer (Name, Age, Phone, Email) values('".$Name."','".$Age."','".$Phone."','".$Email."')";
     $cn = new DBConnector();
     $return = $cn->execStatement($sql);
     header('location:index.php');
-    } 
-    else{
+    } elseif (isset($_POST['sua'])) {
+    $ID=$_GET['ID'];
+    $Name=$_POST['Name'];
+    $Age=$_POST['Age'];
+    $Phone=$_POST['Phone'];
+    $Email=$_POST['Email'];
+    $sql = "UPDATE customer SET Name = '".$Name."', Age = '".$Age."', Phone = '".$Phone."', Email =  '".$Email."' WHERE ID = $ID"; 
+    $cn = new DBConnector();
+    $return = $cn->execStatement($sql);
+    header('location:index.php');   
+    }else{
     $ID=$_GET['ID'];
     $sql="DELETE FROM customer WHERE ID = $ID";
     $cn = new DBConnector();
